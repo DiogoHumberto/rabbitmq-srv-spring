344c36f71418579a1e8bbbe07624fc25aabb859d
Diogo Humberto Vitório Dal Bosco <diogo.bosco@solutis.com.br>
2024-04-02 21:44:31 -0300

    criando estrutura base e docker compose rabbit

---
+++ srv-bco-pgto/src/main/java/com/study/java/srvbcopgto/SrvBcoPgtoApplication.java
@@ -0,0 +1,13 @@
+package com.study.java.srvbcopgto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvBcoPgtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvBcoPgtoApplication.class, args);
+	}
+
+}
